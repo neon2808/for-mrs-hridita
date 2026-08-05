1c7ee350c7fc6244c1013c77c14bcf23bfcc6929
neon2808 <bmnuruzzaman24@gmail.com>
2026-08-05 16:53:41 +0600

    add pass functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// Wait for the loading screen
+setTimeout(function () {
 
+    document.getElementById("loading-screen").style.display = "none";
+
+    document.getElementById("app").style.display = "flex";
+
+}, 4000);
+
+
+// Password
+
+function checkPassword(){
+
+let pass = document.getElementById("password").value;
+
+if(pass==="2808"){
+
+alert("Welcome, Mrs. Hridita ❤️");
+
+}else{
+
+alert("Wrong password 💔");
+
+}
+
+}
